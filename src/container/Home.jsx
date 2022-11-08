7bd1cb44acc27699bfd10399eeda3458c5e4325a
John Ayling <john.ayling@digintent.com>
2022-11-08 07:16:14 -0600

    Add base home page.

---
+++ src/container/Home.jsx
@@ -0,0 +1,51 @@
+import { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import logo from '../assets/logo.png';
+import { Pins } from './Pins';
+import { Sidebar, UserProfile } from '../components';
+import { client } from '../client';
+import { userQuery } from '../utils/data';
+
+const Home = () => {
+	const [toggleSidebar, setToggleSidebar] = useState(false);
+	const [user, setUser] = useState(undefined);
+
+	useEffect(() => {
+		const localUser = JSON.parse(localStorage.getItem('user'));
+
+		if (localUser) {
+			client.fetch(userQuery(localUser._id)).then(data => setUser(data[0]));
+		}
+	}, []);
+
+	console.log(toggleSidebar);
+	return (
+		<div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
+			<div className='hidden md:flex h-screen flex-initial'>
+				<Sidebar user={user} />
+			</div>
+			<div className='flex md:hidden flex-row'>
+				<HiMenu className='cursor-pointer' fontSize={40} onClick={() => setToggleSidebar(true)} />
+				<Link to='/'>
+					<img src={logo} alt='logo' className='w-28' />
+				</Link>
+				<Link to={`user-profile/${user?._id}`}>
+					<img src={user?.image} alt='logo' className='w-28' />
+				</Link>
+			</div>
+			{toggleSidebar && (
+				<div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+					<div className='absolute w-full flex justify-end items-center p-2'>
+						<AiFillCloseCircle className='cursor-pointer' fontSize={30} onClick={() => setToggleSidebar(false)} />
+					</div>
+					<Sidebar user={user} />
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default Home;

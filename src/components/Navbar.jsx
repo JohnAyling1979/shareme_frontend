d6860576535031afbf3d7ba08624df377b5bf9c9
John Ayling <john.ayling@digintent.com>
2022-11-11 09:24:01 -0600

    Setup Nav bar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { IoMdAdd, IoMdSearch } from 'react-icons/io'
 
+const Navbar = ({ user, searchTermRef }) => {
+	const navigate = useNavigate();
 
-const Navbar = () => {
+	if (!user) {
+		return null;
+	}
 
-	return (<div>Navbar</div>);
+	return (
+		<div className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
+			<div className='flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm'>
+				<IoMdSearch className='ml-1' fontSize={21}/>
+				<input
+					className='p-2 w-full bg-white outline-none'
+					type='text'
+					placeholder='Search'
+					onFocus={() => navigate('/search')}
+					onChange={e => searchTermRef.current = e.target.value}
+				/>
+			</div>
+			<div className='flex gap-3'>
+				<Link className='hidden md:block' to={`user-profile/${user._id}`}>
+					<img className='w-14 h-12 rounded-lg' src={user.image} alt='user' />
+				</Link>
+				<Link className='bg-black text-white rounded-lg w-12 md:w-14 h-12 flex justify-center items-center' to='create-pin'>
+					<IoMdAdd />
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
--- src/components/Pins.jsx
+++ src/components/Pins.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import CreatePin from './CreatePin';
 import Feed from './Feed';
@@ -7,12 +7,12 @@ import PinDetail from './PinDetail';
 import Search from './Search';
 
 const Pins = ({ user }) => {
-	const [searchTerm, setSearchTerm] = useState('');
+	const searchTermRef = useRef('');
 
 	return (
 		<div className='px-2 md:px-5'>
 			<div className='bg-gray-50'>
-				<Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+				<Navbar user={user} searchTermRef={searchTermRef} />
 			</div>
 			<div className='h-full'>
 				<Routes>
@@ -20,7 +20,7 @@ const Pins = ({ user }) => {
 					<Route path='/category/:categoryId' element={<Feed />} />
 					<Route path='/pin-detail/:pinId' element={<PinDetail user={user} />} />
 					<Route path='/create-pin' element={<CreatePin user={user} />} />
-					<Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+					<Route path='/search' element={<Search searchTermRef={searchTermRef} />} />
 				</Routes>
 			</div>
 		</div>
